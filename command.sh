34fa0bc5820460ee3f77c76ee9b7c93586a59f8d
smallyunet <dev.wangyu@gmail.com>
2025-10-25 10:23:19 +0800

    add script from mnemonic

--- command.sh
+++ command.sh
@@ -4,3 +4,12 @@ node wallet-gen.js \
   --modes mnemonic-generate,keystore \
   --keystore-password "123456"
 
+node wallet-gen-from-mnemonic.js \
+  --mnemonic "goose music bench regular globe sure rabbit novel tree country aspect insect" \
+  --count 5 \
+  --out-dir ./out_existing \
+  --keystore-password "123456"
+
+node unlock-keystore-verify.js \
+  --in-dir ./out_existing/keystore \
+  --password "123456"
